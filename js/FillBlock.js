e937ab43fdc6ca58ccf57b8acef3372d412a9138
jasonbutler <jason.butler@spongeuk.com>
2016-10-24 17:17:37 +0100

    Update FillBlock.js

--- js/FillBlock.js
+++ js/FillBlock.js
@@ -9,6 +9,13 @@ FillBlock = function (game, x, y){
     this.fillTimer = 0;
     this.filling = true;
     this.fillThreshold = .3;
+    this.maxSpeed = 20000;
+
+    this.threshRect = this.game.add.sprite(0, 0 , "blockSprite")
+    this.threshRect.anchor.setTo(0.5,1)
+    this.threshRect.alpha = 0.5;
+    this.threshRect.scale.y = this.fillThreshold;
+    this.addChild(this.threshRect)
 
     this.fillRect = this.game.add.sprite(0, 0 , "blockSprite")
     this.fillRect.anchor.setTo(0.5,1)
@@ -50,20 +57,6 @@ FillBlock.prototype.update = function(){
             this.fillAmount = this.fillRect.scale.y;
             this.checkFillAmount();
         }
-        // if(this.game.time.now > this.fillTimer){
-        //     this.fillTimer = this.game.time.now + this.fillRate;
-        //     //this.fillAmount += 0.025;
-        //     //this.fillRect.scale.y = this.fillAmount;
-        //     this.checkFillAmount();
-        //     console.log("increasing fill amount to: " + this.fillAmount)
-        //     console.log("this.game.time.now: " + this.game.time.now)
-        //     console.log("this.fillTimer: " + this.fillTimer)
-        //     if(this.fillAmount > 1){
-        //         this.filling = false;
-        //         this.fillRect.tint = 0xFF0000;
-        //         this.overfilled();
-        //     }
-        // }
     }
 
 };
@@ -75,8 +68,7 @@ FillBlock.prototype.stopFill = function(){
     this.filling = false;
     this.fillTween.stop();
     this.fillTween.pendingDelete = false;
-    //sendsignal for score
-    //console.log(this.fillAmount)
+
     var twoPlacedFloat = parseFloat(this.fillAmount * 100).toFixed(2);
     var percInt = Math.round(this.fillAmount * 100);
     this.stopSignal.dispatch(percInt);
@@ -87,7 +79,7 @@ FillBlock.prototype.restartFill = function(){
 
     this.fillAmount = this.fillRect.scale.y = 0;
     this.fillRect.tint = 0xFFFFFF;
-    this.fillRate = this.game.rnd.integerInRange(7500, 20000);
+    this.fillRate = this.game.rnd.integerInRange(7500, this.maxSpeed);
     this.filling = true;
 
     this.fillTween.start()
@@ -95,13 +87,32 @@ FillBlock.prototype.restartFill = function(){
     //console.log("restart the fill tween")
 };
 
+FillBlock.prototype.resetFill = function(){
+
+    this.fillRect.tint = 0xFFFFFF;
+    this.resetTween.start()
+};
+
+FillBlock.prototype.resetBlockStatus = function(){
+    this.fillTween = null;
+    this.fillTween = this.game.add.tween(this.fillRect.scale).to({y:1.1},this.fillRate,Phaser.Easing.Quadratic.Out,true,1000);
+    this.inputEnabled = true;
+    this.fillThreshold = .3;
+    this.threshRect.scale.y = this.fillThreshold;
+    this.fillRect.alpha = 1;
+    this.maxSpeed = 20000;
+    this.restartFill();
+
+};
+
 FillBlock.prototype.overfilled = function(){
     this.fillTween.stop();
+
     this.inputEnabled = false;
     this.fillRect.tint = 0xFF0000;
     this.fillRect.alpha = .75;
 
-    console.log("overfilled!!!! " + this.fillRect.tint)
+console.log("overfilled!!!! " + this.fillRect.tint)
 
     this.fullSignal.dispatch();
     this.emitter.start(true, 800, null, 20);
@@ -122,8 +133,14 @@ FillBlock.prototype.updateThreshold = function(){
         this.fillThreshold += .05;
     };
 
+    if(this.maxSpeed > 8000){
+        this.maxSpeed -= 500;
+    };
+
+    this.game.add.tween(this.threshRect.scale).to({y:this.fillThreshold},750,Phaser.Easing.Elastic.Out,true);
+
     this.scale.setTo(1.2)
-    this.game.add.tween(this.scale).to({x:1,y:1},350,Phaser.Easing.Elastic.Out,true);
+    this.game.add.tween(this.scale).to({x:1,y:1},750,Phaser.Easing.Elastic.Out,true);
         
 };
 
